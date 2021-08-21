09d8b9394df9388f701c6e39d0c7cd044f27ad33
José Rodriguez <isc.joserodriguez@gmail.com>
2021-08-21 09:55:13 -0600

    Coersion de Datos

--- main.js
+++ main.js
@@ -34,3 +34,42 @@ console.log(condicion, condicion2);
 var variableNull = null;
 
 console.log(variableNull);
+
+//Coerción/Conversión de datos
+//Declaro un tipo de dato numérico
+var num=5;
+console.log(num)
+
+console.log(String(num))
+console.log(typeof String(num))
+console.log((num+''))
+console.log(typeof (num+''))
+
+//Declaro un tipo de dato String
+
+var numero="-150"
+console.log(numero);
+
+console.log(+numero);
+console.log(typeof(+numero));
+
+console.log(Number(numero));
+console.log(typeof(Number(numero)));
+
+
+//Declaro un tipo de dato Numérico
+var condicion2=1;
+//   0 / 1
+//Declaro un tipo de dato String
+var condicion="";
+
+console.log(condicion2)
+console.log(condicion)
+
+console.log(Boolean(condicion2))
+
+console.log(Boolean(condicion))
+
+console.log(!!condicion2)
+
+console.log(!!condicion)
